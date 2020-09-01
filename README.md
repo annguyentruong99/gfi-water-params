54d8559b6fa6a8e2c4031e5435b93770709e858d
An Nguyen <annguyen@Ans-MacBook-Pro.fios-router.home>
2020-09-01 14:44:43 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This component was bootstrapped with [Direflow](https://direflow.io).
+
+# Gfi Ts Water Params
+> This project is created using Direflow
+
+```html
+<gfi-ts-water-params></gfi-ts-water-params>
+```
+
+Use this README to describe your Direflow Component
---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+/**
+ * This is the entry file of the Direflow setup.
+ *
+ * You can add any additional functionality here.
+ * For example, this is a good place to hook into your
+ * Web Component once it's mounted on the DOM.
+ *
+ * !This file cannot be removed.
+ * It can be left blank if not needed.
+ */
+
+import GfiTsWaterParams from './direflow-components/gfi-ts-water-params';
+
+GfiTsWaterParams.then((element) => {
+
+  /**
+   * Access DOM node when it's mounted
+   */
+  console.log('gfi-ts-water-params is mounted on the DOM', element);
+});
