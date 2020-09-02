d58f143fdd860d44abcfae54a8cdd146b883b5f0
An Nguyen <annguyen@Ans-MacBook-Pro.local>
2020-09-02 19:25:29 -0400

    added constructor method to the class model

--- src/direflow-components/models/waterparams.model.ts
+++ src/direflow-components/models/waterparams.model.ts
@@ -4,4 +4,12 @@ export class Water {
 	ammonia: number;
 	nitrite: number;
 	nitrate: number;
+
+	constructor() {
+		this.temp = 0;
+		this.pH = 0;
+		this.ammonia = 0;
+		this.nitrite = 0;
+		this.nitrate = 0;
+	}
 }
