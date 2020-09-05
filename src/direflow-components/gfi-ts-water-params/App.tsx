6170d259ea6c31d675f8c8720ca5a29bf5217389
An Nguyen <annguyen@Ans-MacBook-Pro.fios-router.home>
2020-09-05 13:24:10 -0400

    development branch

--- src/direflow-components/gfi-ts-water-params/App.tsx
+++ src/direflow-components/gfi-ts-water-params/App.tsx
@@ -3,12 +3,12 @@ import React, { FC, useState } from "react";
 import {
 	Typography,
 	Button,
-	TextField,
 	Stepper,
 	Step,
 	StepLabel,
 	StepContent,
 	Avatar,
+	Slider,
 } from "@material-ui/core";
 import {
 	makeStyles,
@@ -71,7 +71,55 @@ const theme = createMuiTheme({
 	},
 });
 
-interface WaterParams {
+const marks = [
+	[
+		{ value: 0 },
+		{ value: 20 },
+		{ value: 37 },
+		{ value: 40 },
+		{ value: 50 },
+		{ value: 70 },
+		{ value: 100 },
+	],
+	[
+		{ value: 6 },
+		{ value: 6.4 },
+		{ value: 6.8 },
+		{ value: 6.6 },
+		{ value: 7 },
+		{ value: 7.2 },
+		{ value: 7.6 },
+	],
+	[
+		{ value: 0 },
+		{ value: 0.25 },
+		{ value: 0.5 },
+		{ value: 1 },
+		{ value: 2 },
+		{ value: 4 },
+		{ value: 8 },
+	],
+	[
+		{ value: 0 },
+		{ value: 0.25 },
+		{ value: 0.5 },
+		{ value: 1 },
+		{ value: 2 },
+		{ value: 4 },
+		{ value: 5 },
+	],
+	[
+		{ value: 0 },
+		{ value: 5 },
+		{ value: 10 },
+		{ value: 20 },
+		{ value: 40 },
+		{ value: 80 },
+		{ value: 160 },
+	],
+];
+
+interface Water {
 	temp: number;
 	pH: number;
 	ammonia: number;
@@ -79,45 +127,27 @@ interface WaterParams {
 	nitrate: number;
 }
 
-const waterParams: WaterParams = {
-	temp: 0,
-	pH: 0,
-	ammonia: 0,
-	nitrite: 0,
-	nitrate: 0,
-};
-
-function getSteps(): [string, string][] {
+function getSteps(): string[] {
 	return [
-		["Water Temperature", "Temperature"],
-		["PH Level", "pH"],
-		["Ammonia Level", "Ammonia"],
-		["Nitrite Level", "Nitrite"],
-		["Nitrate Level", "Nitrate"],
+		"Water Temperature",
+		"PH Level",
+		"Ammonia Level",
+		"Nitrite Level",
+		"Nitrate Level",
 	];
-}
-
-function getStepContent(step: number) {
-	switch (step) {
-		case 0:
-			return `Enter Your Water Temperature In The Input Box Below`;
-		case 1:
-			return `Enter Your pH Reading In The Input Box Below`;
-		case 2:
-			return `Enter Your Ammonia Reading In The Input Box Below`;
-		case 3:
-			return `Enter Your Nitrite Reading In The Input Box Below`;
-		case 4:
-			return `Enter Your Nitrate Reading In The Input Box Below`;
-		default:
-			return "Unknown Step!";
-	}
 }
 
 const WaterParam: FC = () => {
 	const classes = useStyles();
 	const [activeStep, setActiveStep] = useState<number>(0);
 	const [complete, setComplete] = useState<boolean>(false);
+	const [params, setParams] = useState({
+		temp: 0,
+		pH: 0,
+		ammonia: 0,
+		nitrite: 0,
+		nitrate: 0,
+	});
 	const steps = getSteps();
 
 	// Proceed to nexr step
@@ -142,7 +172,9 @@ const WaterParam: FC = () => {
 	};
 
 	// Handle the input when customers enter them
-	const handleChange = (event: any) => {};
+	const handleChange = (event: object): void => {
+		console.log(event);
+	};
 
 	return (
 		<div className={classes.root}>
@@ -151,21 +183,18 @@ const WaterParam: FC = () => {
 				{complete === false ? (
 					<Stepper activeStep={activeStep} orientation="vertical">
 						{steps.map((label, index) => (
-							<Step key={label[0]}>
-								<StepLabel>{label[0]}</StepLabel>
-								<StepContent>
-									<Typography>{getStepContent(index)}</Typography>
-									<form
-										noValidate
-										autoComplete="off"
-										className={classes.inputForm}
-									>
-										<TextField
-											id="standard-basic"
-											label={label[1]}
-											style={{ color: "#007c89" }}
-										/>
-									</form>
+							<Step key={label}>
+								<StepLabel>{label}</StepLabel>
+								<StepContent style={{ width: 300 }}>
+									<Slider
+										color="secondary"
+										step={null}
+										marks={marks[index]}
+										min={marks[index][0]["value"]}
+										max={marks[index][6]["value"]}
+										ThumbComponent="div"
+										valueLabelDisplay="on"
+									/>
 								</StepContent>
 							</Step>
 						))}
@@ -174,6 +203,7 @@ const WaterParam: FC = () => {
 					<div>
 						<Typography variant="h5">
 							Below are your water parameter inputs: <br />
+							Hello :D
 						</Typography>
 					</div>
 				)}
